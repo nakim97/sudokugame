23d9210790322c76311f8ece65f33389867ed776
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-03-13 22:47:13 -0500

    Commit and Update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# sudokugame
+# Sudoku
+
+An implementation of a NP-complete puzzle, Sudoku, that allows players to play the game and have their board solution verified. 
+Implements backtracking to solve the sudoku puzzle and allow for users to verify their board.
+
+## Instructions on how to run the program
+- You can run this project by clicking this demo link: https://sudoku-cs325-nakim.netlify.app/
+- You can run this project by downloading a zip file of the project on github, unzip the file, and opening the "index.html" file in your local browser.
+
+## Instructions on how to play the game
+- Fill in the empty spaces on the board using numbers 1 to 9. You can do so by manually typing in the numbers or using the input arrow keys in the individual cells.
+- There can be no repeating numbers in each row, column, or 3x3 box square.
+- When you are done filling in the board, click the CHECK button to verify your solution. An alert message will pop up letting you know if you got it right or wrong.
+- Click the reset button if you want to clear the board.
+- Click the solution button to reveal the solution.
+
+## Solution 
+
+![solution](solution.PNG)
---
+++ script.js
@@ -0,0 +1,204 @@
+
+// Sudoku Board for one puzzle 
+var board = [
+  [8, 0, 0, 9, 3, 0, 0, 0, 2],
+  [0, 0, 9, 0, 0, 0, 0, 4, 0],
+  [7, 0, 2, 1, 0, 0, 9, 6, 0],
+  [2, 0, 0, 0, 0, 0, 0, 9, 0],
+  [0, 6, 0, 0, 0, 0, 0, 7, 0],
+  [0, 7, 0, 0, 0, 6, 0, 0, 5],
+  [0, 2, 7, 0, 0, 8, 4, 0, 6],
+  [0, 3, 0, 0, 0, 0, 5, 0, 0],
+  [5, 0, 0, 0, 6, 2, 0, 0, 8]
+];
+
+// Check each row to see if there are duplicates/valid input
+function rows(board, row, num) {
+  for (let i = 0; i < 9; i++) {
+    if (board[row][i] == num) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Check each column to see if there are duplicates/valid input
+function columns(board, col, num) {
+  for (let i = 0; i < 9; i++) {
+    if (board[i][col] == num) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Check each 3x3 box grid to see if there are duplicates/valid input 
+function box(board, row, col, num) {
+  for (let i = 0; i < 9; i++) {
+    const x = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+    const y = 3 * Math.floor(col / 3) + i % 3;
+    if (board[x][y] == num) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Check if rules apply and if the board/input is valid (check no duplicate and all space filled for row/column/box)
+function verify(board, row, col, num) {
+  if (rows(board, row, num) &&
+    columns(board, col, num) &&
+    box(board, row, col, num)) {
+    return true;
+  }
+  return false;
+};
+
+
+// Function to solve sudoku board 
+function solve(sudoku) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (sudoku[i][j] == '') {
+        // iterate and loop through board grid
+        for (let num = 1; num <= 9; num++) {
+          if (verify(sudoku, i, j, num)) {
+            sudoku[i][j] = `${num}`;
+            // if valid, return true and fill board
+            if (solve(sudoku)) {
+              return true;
+              // else continue looping 
+            } else {
+              sudoku[i][j] = '';
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+solve(board);
+
+
+// Append Solution to Grid and Display solution
+function solution() {
+  i = 0
+  board.flat().forEach(num => document.querySelectorAll('.cell')[i++].value = num)
+  // Disable user from changing inputs on solution page
+  $('input').prop('disabled', true)
+}
+
+
+// Reset and clears Sudoku Board when reset button is clicked
+function reset() {
+  document.getElementById('cell-01').value = "";
+  document.getElementById('cell-02').value = "";
+  document.getElementById('cell-05').value = "";
+  document.getElementById('cell-06').value = "";
+  document.getElementById('cell-07').value = "";
+
+  // Row 2
+  document.getElementById('cell-10').value = "";
+  document.getElementById('cell-11').value = "";
+  document.getElementById('cell-13').value = "";
+  document.getElementById('cell-14').value = "";
+  document.getElementById('cell-15').value = "";
+  document.getElementById('cell-16').value = "";
+  document.getElementById('cell-18').value = "";
+
+  // Row 3 
+  document.getElementById('cell-21').value = "";
+  document.getElementById('cell-24').value = "";
+  document.getElementById('cell-25').value = "";
+  document.getElementById('cell-28').value = "";
+
+  // Row 4
+  document.getElementById('cell-31').value = "";
+  document.getElementById('cell-32').value = "";
+  document.getElementById('cell-33').value = "";
+  document.getElementById('cell-34').value = "";
+  document.getElementById('cell-35').value = "";
+  document.getElementById('cell-36').value = "";
+  document.getElementById('cell-38').value = "";
+
+  // Row 5
+  document.getElementById('cell-40').value = "";
+  document.getElementById('cell-42').value = "";
+  document.getElementById('cell-43').value = "";
+  document.getElementById('cell-44').value = "";
+  document.getElementById('cell-45').value = "";
+  document.getElementById('cell-46').value = "";
+  document.getElementById('cell-48').value = "";
+
+  // Row 6
+  document.getElementById('cell-50').value = "";
+  document.getElementById('cell-52').value = "";
+  document.getElementById('cell-53').value = "";
+  document.getElementById('cell-54').value = "";
+  document.getElementById('cell-56').value = "";
+  document.getElementById('cell-57').value = "";
+
+  // Row 7
+  document.getElementById('cell-60').value = "";
+  document.getElementById('cell-63').value = "";
+  document.getElementById('cell-64').value = "";
+  document.getElementById('cell-67').value = "";
+
+  // Row 8
+  document.getElementById('cell-70').value = "";
+  document.getElementById('cell-72').value = "";
+  document.getElementById('cell-73').value = "";
+  document.getElementById('cell-74').value = "";
+  document.getElementById('cell-75').value = "";
+  document.getElementById('cell-77').value = "";
+  document.getElementById('cell-78').value = "";
+
+  // Row 9
+  document.getElementById('cell-81').value = "";
+  document.getElementById('cell-82').value = "";
+  document.getElementById('cell-83').value = "";
+  document.getElementById('cell-86').value = "";
+  document.getElementById('cell-87').value = "";
+
+  // Allow users to input new input
+  $('input').prop('disabled', false)
+}
+
+// Function for Check Button 
+function check() {
+  // takes user's input and generates array
+  var grid = [];
+  // locate  row of table and set up array to store data
+  $('#table').find('tr').each(function () {
+    var array = [];
+    // store each input from each table row cell and append to array
+    $(this).find('input').each(function () {
+      array.push($(this).val());
+    });
+    // store inputted array in grid var
+    grid.push(array);
+  });
+
+  // Checks to see if user's input is a valid/invalid solution and sends alert message
+  function validate() {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        // if the solution board array is the same as the user's input array, alert "correct/solved"
+        if (board[i][j] === grid[i][j]) {
+          reset();
+          return "CORRECT";
+        }
+        // if the solution board array is different than the user's input array, alert"incorrect"
+        if (board[i][j] != grid[i][j]) {
+          reset();
+          return "INCORRECT TRY AGAIN";
+        }
+      }
+    }
+  }
+  //send alert message displaying if the user's input is correct or incorrect
+  var message = validate();
+  alert(message);
+}
